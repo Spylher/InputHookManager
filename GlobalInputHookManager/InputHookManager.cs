48ff8567ad03eeeb0a8371f5f81d3423ab6a4b29
Spylher <pedro.pepe.5050@gmail.com>
2024-09-07 01:02:17 -0300

    Update to .Net Core.

--- GlobalInputHookManager/InputHookManager.cs
+++ GlobalInputHookManager/InputHookManager.cs
@@ -7,49 +7,105 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using GlobalInputHookManager.Utils;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace GlobalInputHookManager
 {
     using static WinMessages;
-    using static WindowUtils;
+    //using static WindowUtils;
+    using static KeyboardHook;
 
     public class InputHookManager
     {
-        //public static IntPtr HWND { get; set; } = IntPtr.Zero;
+        public static IntPtr HWND { get; set; } = IntPtr.Zero;
+        public static Dictionary<HotKey, Action<Object>> KeyMappingsPressed = new Dictionary<HotKey, Action<Object>>();
+        public static Dictionary<HotKey, Action<Object>> KeyMappingsReleased = new Dictionary<HotKey, Action<Object>>();
 
-        //public static Dictionary<HotKey, Action<Object>> KeyMappingsPressed = new Dictionary<HotKey, Action<Object>>();
-        //public static Dictionary<HotKey, Action<Object>> KeyMappingsReleased = new Dictionary<HotKey, Action<Object>>();
-        //public static HotKey KeyStart = new HotKey();
-        //public static List<HotKey> AllowedKeys { get; set; } = new List<HotKey>();
-        //public static HotKey KeyPressed = new HotKey();
+        public static Dictionary<Keys, bool> KeyStates = new Dictionary<Keys, bool>();
+        public static List<HotKey> AllowedKeys = new List<HotKey>();
+        public static HotKey KeyPressed = new HotKey();
 
-        //public void Install()
-        //{
-        //    if (KeyboardHookId == IntPtr.Zero)
-        //    {
-        //        KeyboardHookId = SetHook(_proc);
-        //        MouseHookId = SetMouseHook(_mouseProc);
-        //        Enable();
-        //    }
+        public void Install()
+        {
+            if (Id == IntPtr.Zero)
+            {
+                KeyboardHook.Id = SetHook(KeyboardProc);
+                //MouseHookId = SetMouseHook(_mouseProc);
+                //Enable();
+            }
 
-        //    foreach (Keys key in Enum.GetValues(typeof(Keys)))
-        //        KeyStates[key] = false;
-        //}
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+                KeyStates[key] = false;
+        }
 
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        public void Uninstall()
+        {
+            //Disable();
+            UnhookWindowsHookEx(Id);
+            Id = IntPtr.Zero;
+            //UnhookWindowsHookEx(MouseHookId);
+            //MouseHookId = IntPtr.Zero;
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx((int)WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        public void RegisterAction(HotKey hotkey, Action<Object> act, KeyState keyState = KeyState.Released)
+        {
+            UnregisterAction(hotkey, KeyState.Pressed);
+            UnregisterAction(hotkey);
+
+            if (keyState == KeyState.Pressed)
+                KeyMappingsPressed.Add(hotkey, act);
+            else if (keyState == KeyState.Released)
+                KeyMappingsReleased.Add(hotkey, act);
+        }
+
+        public void UnregisterAction(HotKey hotkey, KeyState keyState = KeyState.Released)
+        {
+
+            if (keyState == KeyState.Pressed)
+            {
+                foreach (var keyValuePair in KeyMappingsPressed)
+                    if (keyValuePair.Key.Equals(hotkey))
+                        KeyMappingsPressed.Remove(hotkey);
+
+            }
+            else if (keyState == KeyState.Released)
+            {
+                foreach (var keyValuePair in KeyMappingsReleased)
+                    if (keyValuePair.Key.Equals(hotkey))
+                        KeyMappingsReleased.Remove(hotkey);
+            }
+        }
+
+        public void ClearActions()
+        {
+            KeyMappingsPressed.Clear();
+            KeyMappingsReleased.Clear();
+        }
+        public bool IsKeyDown(Keys key)
+        {
+            return KeyStates[key];
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
         //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
 
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //[return: MarshalAs(UnmanagedType.Bool)]
-        //private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        //[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //private static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
--- GlobalInputHookManager/KeyboardHook.cs
+++ GlobalInputHookManager/KeyboardHook.cs
@@ -1,17 +1,98 @@
-﻿using System;
+﻿using GlobalInputHookManager.Utils;
+using MySpy.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GlobalInputHookManager
 {
-    internal class KeyboardHook
+    using static InputHookManager;
+    using static WinMessages;
+    using static WindowUtils;
+
+    public class KeyboardHook
     {
-        //private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-        //private static LowLevelKeyboardProc _proc = KeyboardHookCallback;
-        //private static IntPtr KeyboardHookId = IntPtr.Zero;
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        public static LowLevelKeyboardProc KeyboardProc = KeyboardHookCallback;
+        public static IntPtr Id = IntPtr.Zero;
+
+        private static IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            var vkCode = Marshal.ReadInt32(lParam);
+            var keyCode = (Keys)vkCode;
+
+            if (keyCode == Keys.LControlKey || keyCode == Keys.RControlKey || keyCode == Keys.ControlKey || keyCode == Keys.Control)
+                KeyPressed.CtrlKeyPressed = true;
+            else if (keyCode == Keys.LShiftKey || keyCode == Keys.RShiftKey || keyCode == Keys.ShiftKey || keyCode == Keys.Shift)
+                KeyPressed.ShiftKeyPressed = true;
+            else if (keyCode == Keys.LMenu || keyCode == Keys.RMenu || keyCode == Keys.Menu || keyCode == Keys.Alt)
+                KeyPressed.AltKeyPressed = true;
+            else
+                KeyPressed.MainKey = keyCode;
 
 
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))  // WM_KEYDOWN
+            {
+                KeyStates[keyCode] = true;
+
+                foreach (var HK in KeyMappingsPressed)
+                {
+                    bool isSubset = KeyPressed.Equals(HK.Key);
+
+                    if (isSubset)
+                    {
+                        if (AllowedKeys.Contains(KeyPressed))
+                            HK.Value.Invoke(KeyPressed.MainKey);
+                        else if (IsActiveWindow(HWND)) //other key invoke just if app is visible
+                        {
+                            HK.Value.Invoke(KeyPressed.MainKey);
+                            return (IntPtr)1; // suppress_key
+                        }
+                    }
+                }
+
+                foreach (var HK in KeyMappingsReleased)
+                    if (KeyPressed.Equals(HK.Key) && IsActiveWindow(HWND))
+                        return (IntPtr)1;
+            }
+            else if (nCode >= 0 && (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP)) // WM_KEYUP
+            {
+                KeyStates[keyCode] = false;
+
+                foreach (var HK in KeyMappingsReleased)
+                {
+                    //MessageBox.Show($"pressed{KeyPressed},  list {HK.Key}");
+                    bool isSubset = KeyPressed.Equals(HK.Key);
+
+                    if (isSubset)
+                    {
+                        //keyStart -> invoke function of keystart
+                        //if (KeyStart.MainKey.ToString() == KeyPressed.ToString())
+                        if (AllowedKeys.Contains(KeyPressed))
+                        {
+                            HK.Value.Invoke(KeyPressed.MainKey);
+                            break;
+                        }
+                        else if (IsActiveWindow(HWND))
+                        {
+                            HK.Value.Invoke(KeyPressed.MainKey);
+                            return (IntPtr)1; // suppress_key
+                        }
+                    }
+                }
+
+                KeyPressed.Clear();
+            }
+
+            return CallNextHookEx(Id, nCode, wParam, lParam);
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
     }
 }
